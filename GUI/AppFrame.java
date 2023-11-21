55507f67d36a14d5a0cf6b4f523b2d09574f5f1d
ErickBertoti <erick_bertoti@estudante.sc.senai.br>
2023-11-20 21:50:35 -0300

    Cadastro foi de berço

--- GUI/AppFrame.java
+++ GUI/AppFrame.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 
@@ -20,21 +23,32 @@ public class AppFrame extends JFrame {
         setLayout(null);
 
          JButton botaoCriar = new JButton("Criar Cadastro");
-        botaoCriar.setBounds(100, 200, 250, 70);
+         botaoCriar.setBounds(600, 20, 200, 30);
          add(botaoCriar);
 
+
+         botaoCriar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Ao clicar no botão, instancia e exibe o formulário de cadastro
+                new FormularioCadastro().setVisible(true);
+            }
+        });
+
+
+
          JButton botaoEditar = new JButton("Editar Cadastro");
-        botaoEditar.setBounds(100, 200, 250, 70);
+         botaoEditar.setBounds(800, 20, 200, 30);
          add(botaoEditar);
 
          JButton botaoDeletar = new JButton("Deletar Cadastro");
-        botaoDeletar.setBounds(100, 200, 250, 70);
+         botaoDeletar.setBounds(1000, 20, 200, 30);
          add(botaoDeletar);
 
 
 
 
-       
+       setVisible(true);
     }
     
 }
